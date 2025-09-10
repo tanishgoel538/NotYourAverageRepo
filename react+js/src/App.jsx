40d808b4d67947309339b6c91b102240cde17898
Tanish Goel <tanish.tanish11@gmail.com>
2025-09-10 12:07:24 +0530

    reducer added

--- react+js/src/App.jsx
+++ react+js/src/App.jsx
@@ -1,15 +1,11 @@
 import { motion } from "motion/react"
 import ScrollTriggered from "./components/FramerComponents/Scroll";
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import { useReducer } from "react";
+
 
 const App = () => {
+  const [tasks, dispatch] = useReducer(formReducer, initialTasks);
+  console.log("this is task",tasks)
   return (
     <>
       <motion.div
@@ -67,3 +63,37 @@ const box = {
   backgroundColor: "#9911ff",
   borderRadius: 5,
 }
+
+function formReducer(tasks, action) {
+  switch(action.type) {
+   case 'added': {
+      return [...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false
+      }];
+    }
+    case 'changed': {
+      return tasks.map(t => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter(t => t.id !== action.id);
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false }
+];
